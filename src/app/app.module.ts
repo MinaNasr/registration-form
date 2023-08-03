6fec8afe69a30a34ed6a2f9d0577ae7b1209c54b
mhecontainer-xchange.com <mhe@container-xchange.com>
2023-08-03 18:33:56 +0200

    add angular material lib and create registration form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    RegistrationFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule
+
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
